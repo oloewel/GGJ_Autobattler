ff44fdd244a91d6dacf15bb70f5860c3be58dec3
Ment0ri <o.loewel@mail.de>
2026-01-31 13:30:59 +0100

    Platzierungen mit Klick-Farbwechsel hinzugefügt

---
+++ Assets/Scipts/Platzierungen.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Platzierungen : MonoBehaviour
+{
+    public bool selected;
+
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color selectedColor = Color.red;
+
+    private Renderer rend;
+
+    private void Awake()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown() 
+    {
+        if (selected)
+        {
+            selected = false;
+            rend.material.color = selected ? selectedColor : normalColor;
+        }
+        else
+        {
+            selected = true;
+            rend.material.color = selected ? selectedColor : normalColor;
+        }
+    }
+
+}
